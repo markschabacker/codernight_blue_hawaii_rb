ae5b6ef903df9200fdb14adbcfe7e63649b4b14e
Mark Schabacker <mark.schabacker@gmail.com>
2013-06-12 19:03:39 -0400

    Add be_a_full_year matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 require 'rspec'
 
 require File.join(File.dirname(__FILE__), '../rate_calculator.rb')
+
+RSpec::Matchers.define :be_a_full_year do
+  match do |actual|
+    actual.start_day.should == 1
+    actual.start_month.should == 1
+    actual.end_day.should == 31
+    actual.end_month.should == 12
+  end
+end
