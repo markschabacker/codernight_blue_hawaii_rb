61cda31ab3269ad59bc2bc12004e4ea89a8c1189
Mark Schabacker <mark.schabacker@gmail.com>
2013-06-12 19:31:28 -0400

    Add (pretty nasty) UnitHydrator for single units

--- rate_calculator.rb
+++ rate_calculator.rb
@@ -1,4 +1,5 @@
 require 'JSON'
+require 'bigdecimal'
 
 class RateCalculator
   def calculate_rates(units_json_string, reservation_string, tax_rate)
@@ -18,6 +19,49 @@ class RateCalculator
 end
 
 class UnitHydrator
+  def hydrate_json(units_json_string)
+    ret_array = []
+    return ret_array if units_json_string.nil? || (0 == units_json_string.length)
+
+    unit_dict_array = JSON.parse(units_json_string)
+    unit_dict_array.each do |unit_dict|
+      name = unit_dict["name"]
+
+      seasons = []
+
+      json_seasons = unit_dict["seasons"]
+      unless json_seasons.nil?
+        json_seasons.each do |json_season_dict|
+          json_season_dict.values.each do |json_season|
+            rate = BigDecimal.new(json_season["rate"][1..-1])
+            start_month, start_day = json_season["start"].split("-").map { |int_str| int_str.to_i }
+            end_month, end_day = json_season["end"].split("-").map { |int_str| int_str.to_i }
+
+            if(end_month < start_month) || ((end_month == start_month) && (end_day < start_day))
+              seasons << Season.new(1, 1, end_month, end_day, rate)
+              seasons << Season.new(start_month, start_day, 12, 31, rate)
+            else
+              seasons << Season.new(start_month, start_day, end_month, end_day, rate)
+            end
+          end
+        end
+      else
+        rate = BigDecimal.new(unit_dict["rate"][1..-1])
+        seasons << Season.new(1, 1, 12, 31, rate)
+      end
+
+      cleaning_fee_string = unit_dict["cleaning fee"]
+      if cleaning_fee_string.nil?
+        cleaning_fee = 0
+      else
+        cleaning_fee = BigDecimal.new(cleaning_fee_string[1..-1])
+      end
+
+      ret_array << Unit.new(name, seasons, cleaning_fee)
+    end
+
+    ret_array
+  end
 end
 
 class Unit
---
+++ spec/lib/unit_hydrator_spec.rb
@@ -0,0 +1,123 @@
+require_relative '../spec_helper.rb'
+
+describe "The Unit Hydrator" do
+  before do
+    @unit_hydrator = UnitHydrator.new
+  end
+
+  describe "hydrating units" do
+    [nil, ""].each do |empty_value|
+      it "returns no units for empty JSON (#{empty_value || 'nil'})" do
+        ret_val = @unit_hydrator.hydrate_json(empty_value)
+        ret_val.should_not be_nil
+        ret_val.count.should == 0
+      end
+    end
+
+    describe "reading a single unit" do
+      it "reads a single unit" do
+        units_json_string = %{[{"name":"doesntmatter","rate":"$42"}]}
+        unit_results = @unit_hydrator.hydrate_json(units_json_string)
+        unit_results.should_not be_nil
+        unit_results.count.should == 1
+      end
+
+      it "reads the unit name" do
+        unit_name = "Paradise Inn"
+        units_json_string = %{[{"name":"#{unit_name}","rate":"$42"}]}
+        unit_results = @unit_hydrator.hydrate_json(units_json_string)
+        unit_results[0].name.should == unit_name
+      end
+
+      describe "when cleaning fee is specified" do
+        it "reads the cleaning fee" do
+            cleaning_fee = 120
+            units_json_string = %{[{"name":"doesntmatter","rate":"$42","cleaning fee":"$#{cleaning_fee}"}]}
+            unit_results = @unit_hydrator.hydrate_json(units_json_string)
+            unit_results[0].cleaning_fee.should == cleaning_fee
+        end
+      end
+
+      describe "when cleaning fee is not specified" do
+        it "sets cleaning fee to 0" do
+            units_json_string = %{[{"name":"doesntmatter","rate":"$42"}]}
+            unit_results = @unit_hydrator.hydrate_json(units_json_string)
+            unit_results[0].cleaning_fee.should == 0
+        end
+      end
+
+      describe "when no seasons are specified" do
+        it "returns a unit with 1 full year season" do
+          unit_rate = 250
+          units_json_string = %{[{"name":"doesntmatter","rate":"$#{unit_rate}"}]}
+          unit_results = @unit_hydrator.hydrate_json(units_json_string)
+
+          unit = unit_results[0]
+          unit.seasons.should_not be_nil
+          unit.seasons.count.should == 1
+
+          unit.seasons[0].should be_a_full_year
+          unit.seasons[0].rate.should == unit_rate
+        end
+      end
+
+      describe "when two seasons are specified" do
+        it "returns a unit with two seasons if seasons do not overlap new years" do
+          unit_1_rate = 137
+          unit_2_rate = 220
+
+          units_json_string = %{[{"name":"doesntmatter","seasons":[{"one":{"start":"01-01","end":"01-13","rate":"$#{unit_1_rate}"}},{"two":{"start":"01-14","end":"12-31","rate":"$#{unit_2_rate}"}}]}]}
+
+          unit_results = @unit_hydrator.hydrate_json(units_json_string)
+
+          unit = unit_results[0]
+          unit.seasons.should_not be_nil
+          unit.seasons.count.should == 2
+
+          unit.seasons[0].start_month.should == 1
+          unit.seasons[0].start_day.should == 1
+          unit.seasons[0].end_month.should == 1
+          unit.seasons[0].end_day.should == 13
+          unit.seasons[0].rate.should == unit_1_rate
+
+          unit.seasons[1].start_month.should == 1
+          unit.seasons[1].start_day.should == 14
+          unit.seasons[1].end_month.should == 12
+          unit.seasons[1].end_day.should == 31
+          unit.seasons[1].rate.should == unit_2_rate
+        end
+
+        it "returns a unit with three seasons if seasons overlap new years" do
+          unit_1_rate = 137
+          unit_2_rate = 220
+
+          units_json_string = %{[{"name":"doesntmatter","seasons":[{"one":{"start":"01-15","end":"01-30","rate":"$#{unit_1_rate}"}},{"two":{"start":"01-31","end":"01-14","rate":"$#{unit_2_rate}"}}]}]}
+
+          unit_results = @unit_hydrator.hydrate_json(units_json_string)
+
+          unit = unit_results[0]
+          unit.seasons.should_not be_nil
+          unit.seasons.count.should == 3
+
+          unit.seasons[0].start_month.should == 1
+          unit.seasons[0].start_day.should == 15
+          unit.seasons[0].end_month.should == 1
+          unit.seasons[0].end_day.should == 30
+          unit.seasons[0].rate.should == unit_1_rate
+
+          unit.seasons[1].start_month.should == 1
+          unit.seasons[1].start_day.should == 1
+          unit.seasons[1].end_month.should == 1
+          unit.seasons[1].end_day.should == 14
+          unit.seasons[1].rate.should == unit_2_rate
+
+          unit.seasons[2].start_month.should == 1
+          unit.seasons[2].start_day.should == 31
+          unit.seasons[2].end_month.should == 12
+          unit.seasons[2].end_day.should == 31
+          unit.seasons[2].rate.should == unit_2_rate
+        end
+      end
+    end
+  end
+end
