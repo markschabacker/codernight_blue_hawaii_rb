f22751c29e4a5711008489d2face6215c6737201
Mark Schabacker <mark.schabacker@gmail.com>
2013-06-15 20:18:44 -0400

    Convert Season spec to let statements

--- spec/lib/season_spec.rb
+++ spec/lib/season_spec.rb
@@ -1,15 +1,12 @@
 require_relative '../spec_helper.rb'
 
 describe "Season" do
-  before do
-    @start_day_of_year = :start_day_of_year
-    @end_day_of_year = :end_day_of_year
-    @rate = :rate
+  let (:start_day_of_year) { :start_day_of_year }
+  let (:end_day_of_year) { :end_day_of_year }
+  let (:rate) { :rate }
+  let (:season) { Season.new(start_day_of_year, end_day_of_year, rate) }
 
-    @season = Season.new(@start_day_of_year, @end_day_of_year, @rate)
-  end
-
-  subject { @season }
+  subject { season }
 
   it {should respond_to(:start_day_of_year) }
   it {should_not respond_to(:start_day_of_year=) }
@@ -21,14 +18,14 @@ describe "Season" do
   it {should_not respond_to(:rate=) }
 
   it "can be instantiated" do
-    Season.new(@start_day_of_year, @end_day_of_year, @rate).should_not be_nil
+    Season.new(start_day_of_year, end_day_of_year, rate).should_not be_nil
   end
 
   it "can be instantiated and set property values" do
-    @season = Season.new(@start_day_of_year, @end_day_of_year, @rate)
+    season = Season.new(start_day_of_year, end_day_of_year, rate)
 
-    @season.start_day_of_year.should == @start_day_of_year
-    @season.end_day_of_year.should == @end_day_of_year
-    @season.rate.should == @rate
+    season.start_day_of_year.should == start_day_of_year
+    season.end_day_of_year.should == end_day_of_year
+    season.rate.should == rate
   end
 end
