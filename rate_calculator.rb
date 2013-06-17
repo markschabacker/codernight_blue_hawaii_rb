55e26ac7f7948dd87e91fd713ed33e0d47522989
Mark Schabacker <mark.schabacker@gmail.com>
2013-06-17 19:17:37 -0400

    Fix integration test

--- rate_calculator.rb
+++ rate_calculator.rb
@@ -7,12 +7,12 @@ class RateCalculator
     units = unit_hydrator.hydrate_json(units_json_string)
 
     reservation_range_generator = ReservationRangeGenerator.new
-    reservation_range = reservation_range_generator.generate_date_range(reservation_string)
+    reservation_range = reservation_range_generator.generate(reservation_string)
 
     unit_rates = {}
     units.each do |unit|
       unit_rate_calculator = UnitRateCalculator.new(unit, reservation_range, tax_rate)
-      unit_rates[unit.name] = unit_rate_calculator.calculate_rate
+      unit_rates[unit.name] = unit_rate_calculator.calculate
     end
     unit_rates
   end
--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -6,13 +6,13 @@ describe "The Rate Calculator (Integration)" do
 
     json_units = '[{"name":"Fern Grove Lodge","seasons":[{"one":{"start":"05-01","end":"05-13","rate":"$137"}},{"two":{"start":"05-14","end":"04-30","rate":"$220"}}],"cleaning fee":"$98"}]'
     reservation_string = "2011/05/07 - 2011/05/20"
-    tax_rate = 1.0411416
+    tax_rate = 1.05
 
     dict_results = rate_calculator.calculate_rates(json_units, reservation_string, tax_rate)
 
     dict_results.should_not be_nil
     dict_results.count.should == 1
-    dict_results[0].name.should == "Fern Grove Lodge"
-    dict_results[0].cost.should == 2474.79
+    dict_results.keys[0].should == "Fern Grove Lodge"
+    dict_results.values[0].should == 2495.85
   end
 end
