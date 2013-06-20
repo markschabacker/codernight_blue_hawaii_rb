4289624108eedbb1555003306d0229f8b0915db5
Mark Schabacker <mark.schabacker@gmail.com>
2013-06-19 20:34:16 -0400

    Git rid of unnecessary equal_season rspec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,17 +2,6 @@ require 'rspec'
 
 require File.join(File.dirname(__FILE__), '../rate_calculator.rb')
 
-RSpec::Matchers.define :equal_season do |expected|
-  match do |actual|
-    # TODO: just override equality operator?
-    actual.start_day_of_year.day.should == expected.start_day_of_year.day
-    actual.start_day_of_year.month.should == expected.start_day_of_year.month
-    actual.end_day_of_year.day.should == expected.end_day_of_year.day
-    actual.end_day_of_year.month.should == expected.end_day_of_year.month
-    actual.rate.should == expected.rate
-  end
-end
-
 RSpec::Matchers.define :be_a_full_year do
   match do |actual|
     actual.start_day_of_year.day.should == 1
